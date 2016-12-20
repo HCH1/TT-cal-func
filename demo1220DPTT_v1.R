69c70951aa8ee00f68ffe1920f95255f97f21a5a
HCH1 <theone_730801@hotmail.com>
2016-12-20 17:11:49 +0800

    Update demo1220DPTT_v1.R

--- demo1220DPTT_v1.R
+++ demo1220DPTT_v1.R
@@ -6,9 +6,9 @@
 ## http://seananderson.ca/2013/10/19/reshape.html
 #mx_dtt = read.csv(file1, header = TRUE)
 ##
-#demo1220DPTT_v1 <- function(file1){
-## e.g. demo1209DTT_lay_can_useV1("28LPSe_Rev1.0_1.0_Truth_Tables_DPTTn2v2.csv")
-   
+demo1220DPTT_v1 <- function(file1){
+## e.g. demo1220DPTT_v1("28LPSe_Rev1.0_1.0_Truth_Tables_DPTTn2v2.csv")
+## mx_dptt = read.csv(file1, header = TRUE)
 mx_dptt = read.csv("28LPSe_Rev1.0_1.0_Truth_Tables_DPTTn2v2.csv", header = TRUE)
 # unpivot by library(reshape2)
 mx_dptt2 <- melt(mx_dptt, id = "Device", na.rm = TRUE)
@@ -31,27 +31,49 @@ write.csv(x = mx_dptt3, file = "o1220_mx_dptt3.csv")
 ## if want to filter multi cols -> use paste(a, b) =="?"
 	mx_dptt2_fil <- mx_dptt2
 	mx_dptt2_fil <- mx_dptt2_fil[ mx_dptt2_fil[[3]] != "x",]
-	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "NW|0",]
-	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "X3BF|1",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "NW|0",] # CN -> 1 is open.
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "X3BF|1",] # CP -> 0 is open.
 	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "X4IN|1",]
 	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "X3NV|0",]
 	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "N3|0",]
 	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "IP|0",]
 	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "PV|0",]
 	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "HG|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "ZG|1",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "ZP|1",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "HG|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "ZN|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "HN|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "HP|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "BH|1",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "PH|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "XW|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "LW|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "X9BH|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "X9PH|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "X4BH|1",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "X4PH|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "GN|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "GP|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "XH|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "BH|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "BN|0",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "OP|1",]
+	mx_dptt2_fil <- mx_dptt2_fil[ paste( mx_dptt2_fil[[2]], mx_dptt2_fil[[3]], sep = "|") != "TJ|0",]
 
 	write.csv(x = mx_dptt2_fil, file = "o1220_mx_dptt2_fil.csv")
 ##
-  
-    mx_dtt2_val1 <- mx_dtt2[mx_dtt2$value == "1",]
-    mx_dtt2_val2 <- mx_dtt2_val1[mx_dtt2_val1$Device == mx_dtt2_coldev[i],]
+mx_dptt2_fil2 <- mx_dptt2_fil
+
+  for( i in 1:length(mx_dptt2_coldev) ){
+# length(mx_dptt2_coldev) 140ea
+    mx_dptt2_fil2 <- mx_dptt2_fil2[mx_dptt2_fil2[[1]] == mx_dptt2_coldev[i],]
     ## use data.frame will be easy to cbind
-    df1 <- data.frame(mx_dtt2_val2$Device[1])
-    df2 <- data.frame(mx_dtt2_val2$SPICE[1])
-    df3 <- t(data.frame(mx_dtt2_val2$variable))
+    df1 <- data.frame(mx_dptt2_fil2$Device[1])
+    df3 <- t(data.frame(mx_dptt2_fil2[[2]]))
     ## / cant be filename. use gsub(x) to replace
-    write.csv(x = cbind(df1, df2, df3), file = paste("can_use", file1, "_", gsub("\\/",";", mx_dtt2_coldev[i]), ".csv", sep = "") )
-#  }
-#  return( cbind(df1, df2, df3) )
-#}
+    write.csv(x = cbind(df1, df3), file = paste("DPTT_", file1, "_", gsub("\\/",";", mx_dptt2_coldev[i]), ".csv", sep = "") )
+  }
+  return("ok")
+}
 ##
